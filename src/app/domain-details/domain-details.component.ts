ec8a4d4f5bb19a52e792de6a652fe193028009d5
User <user@hackerrank.workspace>
2026-01-11 21:48:43 +0000

    Add initial repository
    Commit by HackerRank

--- src/app/domain-details/domain-details.component.ts
+++ src/app/domain-details/domain-details.component.ts
@@ -1,25 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-domain-details',
   templateUrl: './domain-details.component.html',
   styleUrls: ['./domain-details.component.scss']
 })
-export class DomainDetailsComponent implements OnInit { // Export class
-  domain: any; // Domain object
-
-  constructor(private route: ActivatedRoute, private router: Router) {} // Inject route and router
-
-  ngOnInit() { // On init
-    const id = this.route.snapshot.paramMap.get('id'); // Get domain ID from route
-    // Fetch domain by ID from your data source, example:
-    // this.domain = this.domainService.getDomainById(id);
-    // For demo, use static data:
-    this.domain = { id, name: 'example.com', autoRenew: true, privacy: false }; // Example data
-  }
-
-  backToList() { // Back to list function
-    this.router.navigate(['/']); // Navigate to list
-  }
+export class DomainDetailsComponent {
 }
